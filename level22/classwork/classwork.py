cced4bdc319f2f2f4c93bdb9127c37f57ff55b76
sabakobesashvili <sabakobesashvili@gmail.com>
2024-11-27 22:37:28 +0400

    first commit

--- level22/classwork/classwork.py
+++ level22/classwork/classwork.py
@@ -1,21 +1,21 @@
 # შექმენით სია სადაც შეინახავთ სხვადასხვა ტიპის მონაცემებს და შემდეგ დაპრინტეთ ის.
 
-# type =["saba",0.2,22]
-# print(index)
+type =["saba",0.2,22]
+print(type)
 
-# შექმენით სია სადაც შეინახავთ თქვენი საყვარელ საჭმელს და დაპრინტეთ ის
+#შექმენით სია სადაც შეინახავთ თქვენი საყვარელ საჭმელს და დაპრინტეთ ის
 
-# my_favourite = ["xinkali","lobiani","burger","pizza","shaurma"]
-# print(my_favourite)
+my_favourite = ["xinkali","lobiani","burger","pizza","shaurma"]
+print(my_favourite)
 
 
 
-# შექმენით სია სადაც გექნებათ 10 item და indexing მეშვეობით დაპრინტეთ 4 პოზიციაზე მდგომი
+#შექმენით სია სადაც გექნებათ 10 item და indexing მეშვეობით დაპრინტეთ 4 პოზიციაზე მდგომი
 
-# items = ["pen","pencis","monitor","pc","compiuter","mouse","keybord","camera","mousepad"]
-# print(items[3])
+items = ["pen","pencis","monitor","pc","compiuter","mouse","keybord","camera","mousepad"]
+print(items[3])
 
-# შექმენით სია სადაც გექნებათ 15 მონაცემი და დაპრინტავთ პირველი item-იდან 10 -მდე.
+#შექმენით სია სადაც გექნებათ 15 მონაცემი და დაპრინტავთ პირველი item-იდან 10 -მდე.
 
-# veriable = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-# print(veriable[:10])
+veriable = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print(veriable[:10])
