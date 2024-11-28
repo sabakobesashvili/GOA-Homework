910db26d50203d3cd32325e523e0af266857b187
sabakobesashvili <sabakobesashvili@gmail.com>
2024-11-28 23:17:21 +0400

    first commit

---
+++ level24/hmework/homework.py
@@ -0,0 +1,17 @@
+list = [5, 10, 15, 20, 25, 30, 35, 40]
+slice = list[0:4]
+slice = list[4:7]
+print(slice)
+
+slice = list[2:5]
+print(slice)
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+slice = letters[1::2]
+print(slice)
+
+slice = letters[::-1]
+print(slice)
+
+slice = letters[2:5]
