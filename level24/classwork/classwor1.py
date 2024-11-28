5b87953a2a87699b217fac8bb2fc6f413b371b78
sabakobesashvili <sabakobesashvili@gmail.com>
2024-11-28 22:38:52 +0400

    first commit

---
+++ level24/classwork/classwor1.py
@@ -0,0 +1,8 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+# slice = list[:3]
+# print(slice)
+
+slice = list[5:7]
+print(slice)
+
+
---
+++ level24/classwork/classwork.py
@@ -0,0 +1,12 @@
+list = [1,2,3,4,5,6,7,9]
+print(list)
+list[7] = 8
+print(list)
+
+list = [1,2,3,4]
+print(list[2])
+
+x = "sience"
+print(x)
+x = x[3] + x[1] + x[4] + x[2]
+print(x)
