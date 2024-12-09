075957d3ed744eecfbfaaf2d256b4788d9caa27a
sabakobesashvili <sabakobesashvili@gmail.com>
2024-12-09 22:50:49 +0400

    first commit

---
+++ level20/classwork/classwork.py
@@ -0,0 +1,10 @@
+num = 15 > 12
+num2 = 19 != 20
+num3 = 10 > 2
+num4 = 2 < 4
+num5 = 12 < 18
+num6 = 20 != 20
+ 
+print(num or num2) #დაპრინტავ true ს რადგან ერთი true არის და ერთი false და წერია or
+print(num3 and num4)#დაპრინტავს true ს რადგან ორი true არის და წერია and
+print(num5 or num6)#დაპრინტავვს true ს რადგან არის ერთი true და ერთი false და წერია or
