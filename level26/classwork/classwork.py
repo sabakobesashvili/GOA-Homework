3d1e16843586b7a4b7c07bd98a1552cdf91943e5
sabakobesashvili <sabakobesashvili@gmail.com>
2024-12-24 17:48:10 +0400

    first commit

--- level26/classwork/classwork.py
+++ level26/classwork/classwork.py
@@ -1,19 +1,19 @@
-# list = [1,2,3,4,5,6]
-# print(list[2:4])
+list = [1,2,3,4,5,6]
+print(list[2:4])
 
-# list = [1,2,3,4,5]
-# print(list[0:])
+list = [1,2,3,4,5]
+print(list[0:])
 
-# list = [1,"a",1.0,1>0]
-# print(list[-5:])
+list = [1,"a",1.0,1>0]
+print(list[-5:])
 
-# list = [1,2,3,4,5]
-# list[1] = ["k"]
-# list[2] = ["t"]
-# print(list)
+list = [1,2,3,4,5]
+list[1] = ["k"]
+list[2] = ["t"]
+print(list)
 
-# string = "goalorientedacademy"
-# print(string[0:4])
+string = "goalorientedacademy"
+print(string[0:4])
 
 list = [1,2,3,4,5,6]
 print(list[::-1])
