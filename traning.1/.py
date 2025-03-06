ae1aef68963b16cbb43f8e21684d272176709cab
sabakobesashvili <sabakobesashvili@gmail.com>
2025-03-06 22:55:07 +0400

    .

---
+++ traning.1/.py
@@ -0,0 +1,15 @@
+# password = 2025
+# user_input = int(input("enter pasword"))
+# if password == user_input:
+#     print("access granted")
+# else:
+#     print("access denied")
+
+
+# user_input = int(input("enter number"))
+# if user_input %2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
