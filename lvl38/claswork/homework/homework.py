924de3263c36bcb2a35de125d755593196282484
sabakobesashvili <sabakobesashvili@gmail.com>
2025-03-13 13:13:02 +0400

    .

---
+++ lvl38/claswork/homework/homework.py
@@ -0,0 +1,50 @@
+city = "tbilisi"
+print(city.upper())
+
+
+
+user_input = input("enter your name")
+print(user_input.upper())
+
+
+
+text = "hello word"
+print(text.upper())
+
+
+
+name = "GABRIEL"
+print(name.lower())
+
+user_input = input("enter your text")
+print(user_input.lower())
+
+
+
+text = "PYTHON IS FUN"
+print(text.lower())
+
+name = "davit"
+print(name.capitalize())
+
+user_input = input("enter your text")
+print(user_input.capitalize())
+
+
+
+text = "hello world"
+print(text.capitalize())
+
+
+text = "programming"
+print(text.find("g"))
+
+
+text = "hello world"
+print(text.find("world"))
+
+
+text = "python is fun"
+print(text.find("i"))
+
+
